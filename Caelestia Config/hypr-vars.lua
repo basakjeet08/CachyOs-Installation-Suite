f76944a7b350f8d629724791db72b0caba2fd6b7
Anirban Basak <jeetbasak2002@gmail.com>
2026-07-23 15:55:09 +0530

    Custom configuration files added

---
+++ Caelestia Config/hypr-vars.lua
@@ -0,0 +1,32 @@
+return {
+    ------------------
+    ---- HYPRLAND ----
+    ------------------
+
+    -- Custom Default Apps
+    browser                    = "zen-browser",
+    editor                     = "code",
+
+    -- Custom Touchpad Configs
+    workspaceSwipeFingers      = 3,
+
+    -- Custom Blur Configs
+    blurSpecialWs              = true,
+
+    -- Cutom Gap Configs
+    windowGapsIn               = 2,
+    windowGapsOut              = 8,
+    singleWindowGapsOut        = 8,
+
+    -- Custom Window styling Config
+    windowOpacity              = 0.80,
+    windowBorderSize           = 2,
+
+    ------------------
+    ---- KEYBINDS ----
+    ------------------
+
+    -- Custom Apps Config
+    kbTerminal                 = "SUPER + Return",
+    kbBrowser                  = "SUPER + B",
+}
